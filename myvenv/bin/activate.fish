54b1cc4726d1c4c3219ad180dccf35d3fe44ba9e
Sanika Gupte <sanikagupte@SanikaGuptesMBP.lan>
2022-12-10 15:40:30 -0500

    Added Sanika's Viterbi Code and readData.py files

--- myvenv/bin/activate.fish
+++ myvenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sanikagupte/Library/Mobile Documents/com~apple~CloudDocs/Duke/Fall 2022/ECE684/FinalProject/myvenv"
+set -gx VIRTUAL_ENV "/Users/sanikagupte/Library/Mobile Documents/com~apple~CloudDocs/Duke/Fall 2022/ECE684/FinalProject/pos-tagging/myvenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
